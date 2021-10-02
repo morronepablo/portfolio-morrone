02d1752b1455c953bfee687893efeda7a4067e68
morronepablo <morronepablo@gmail.com>
2021-10-02 02:24:37 -0300

    Agregando mas funciones al portfolio

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
+/* ********** Menu ********** */
 
+/* Función Anónima Auto Ejecutable */
+
+((d) =>{
+    const $btnMenu = d.querySelector(".menu-btn"),
+    $menu = d.querySelector(".menu");
+
+    $btnMenu.addEventListener("click", e=>{
+        $btnMenu.firstElementChild.classList.toggle("none");
+        $btnMenu.lastElementChild.classList.toggle("none");
+        $menu.classList.toggle("is-active");
+    });
+
+    d.addEventListener("click", e=> {
+        if(!e.target.matches(".menu a")) return false;
+
+        $btnMenu.firstElementChild.classList.remove("none");
+        $btnMenu.lastElementChild.classList.add("none");
+        $menu.classList.remove("is-active");
+    })
+})(document);
+
+/* Fin de funcion */
